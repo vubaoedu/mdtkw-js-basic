88fcca4fc7d0a7d0253d914ae979f231403a9f96
Dinh-Bao Vu <vubao.edu@gmail.com>
2021-12-24 21:42:19 +0700

    upgrade game

--- dom/labs/truc-xanh-game/demo-truc-xanh-game/game.js
+++ dom/labs/truc-xanh-game/demo-truc-xanh-game/game.js
@@ -1,3 +1,5 @@
+
+
 const cardArray = [
   {
     name: "fries",
@@ -50,21 +52,75 @@ const cardArray = [
 ];
 
 let dem = 0;
+let card1 = null, card2 = null;
+let diem = 0;
 
+let fail = new Audio("audios/fail.mp3");
+let success = new Audio("audios/success.mp3");
+let win = new Audio("audios/hpny.mp3");
+let accepted = true;
 function createBoard() {
+
+
+  cardArray.sort(function () {
+    return 0.5 - Math.random();
+  });
+  console.log(cardArray);
+
   for (let i = 0; i < cardArray.length; i++) {
     // Tạo ra một thẻ img có src
     let img = document.createElement("img");
     img.src = "./images/blank.png";
+    img.className = cardArray[i].name;
 
     // Thêm sự kiện click cho image
     img.addEventListener("click", function () {
+      console.log(img.className);
       // Thay đổi src của img cho giống với data
-      this.src = cardArray[i].img;
-      dem++;
-      if (dem % 2 == 0) {
-        //alert("Đây là lần thứ 2 bạn click");
+      if (accepted) {
+        this.src = cardArray[i].img;
+        dem++;
+        if (dem % 2 == 0) {
+          accepted = false;
+          card2 = this;
+          if (card2 == card1) {
+            dem--;
+            accepted = true;
+            return;
+          }
+          //alert("Đây là lần thứ 2 bạn click");
+          if (card1.className === card2.className) {
+            success.play();
+            diem++;
+            if (diem == 1) {
+              document.getElementById("wrapper").style.display = "block";
+              setTimeout(function () {
+                win.play();
+              }, 3000);
+              firework();
+            }
+            setTimeout(function () {
+              card1.style.visibility = "hidden";
+              card2.style.visibility = "hidden";
+              accepted = true;
+            }, 1000);
+            document.getElementById("result").innerHTML = diem;
+          }
+          else {
+            fail.play();
+            setTimeout(function () {
+              card1.src = "./images/blank.png";
+              card2.src = "./images/blank.png";
+              accepted = true;
+            }, 1000);
+          }
+        }
+        else {
+
+          card1 = this;
+        }
       }
+
     });
 
     // Lấy #board
@@ -73,6 +129,21 @@ function createBoard() {
     // Đưa thẻ img vào trong #board
     board.appendChild(img);
   }
+}
+
+function firework() {
+  const container = document.querySelector('#wrapper');
+  const fireworks = new Fireworks(container, { /* options */ });
+  fireworks.setOptions({ delay: { min: 10, max: 15 }, sound: { enabled: true } });
+  fireworks.start();
+  //fireworks.pause();
+  //fireworks.clear();
+
+  // stop and clear fireworks
+  //fireworks.stop();
+
+  // after initialization you can change the fireworks parameters
+
 }
 
 createBoard();
